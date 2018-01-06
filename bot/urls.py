7a89aa437548f260ac07cfbb0680c6ea10e85cd5
Evelyne Suto <s.evelyne96@gmail.com>
2018-01-06 04:10:06 +0200

    favicon

--- bot/urls.py
+++ bot/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
      path('', views.index, name='index'),
-     path('post/message', views.post_message, name='post_message')
+     path('post/message', views.post_message, name='post_message'),
+     path(r'^favicon.ico$', RedirectView.as_view( url=staticfiles_storage.url('/favicon.ico') ), name="favicon" ),
 ]
