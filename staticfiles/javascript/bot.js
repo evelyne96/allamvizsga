2428600ef9427bc87dd76870a7a664fcf07ece3b
Evelyne Suto <s.evelyne96@gmail.com>
2018-02-28 16:42:04 +0200

    collect statics

--- staticfiles/javascript/bot.js
+++ staticfiles/javascript/bot.js
@@ -25,4 +25,55 @@ $(document).ready(function () {
   $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
   });  
+
 });
+
+
+function lighten(color, luminosity) {
+	// validate hex string
+	color = new String(color).replace(/[^0-9a-f]/gi, '');
+	if (color.length < 6) {
+		color = color[0]+ color[0]+ color[1]+ color[1]+ color[2]+ color[2];
+	}
+	luminosity = luminosity || 0;
+
+	// convert to decimal and change luminosity
+	var newColor = "#", c, i, black = 0, white = 255;
+	for (i = 0; i < 3; i++) {
+		c = parseInt(color.substr(i*2,2), 16);
+		c = Math.round(Math.min(Math.max(black, c + (luminosity * white)), white)).toString(16);
+		newColor += ("00"+c).substr(c.length);
+	}
+	return newColor; 
+}
+
+
+
+function setBackgroundColor(orientation,colorOne, colorTwo){
+  var dom = document.getElementById('bg');
+  var orientation = "bottom right";
+  dom.style.backgroundImage =  getCssValuePrefix() + 'linear-gradient('+ orientation + ', ' + colorOne + ', ' + colorTwo + ')';
+}
+
+function getCssValuePrefix()
+{
+    var rtrnVal = '';//default to standard syntax
+    var prefixes = ['-o-', '-ms-', '-moz-', '-webkit-'];
+    // Create a temporary DOM object for testing
+    var dom = document.createElement('div');
+    for (var i = 0; i < prefixes.length; i++)
+    {
+        // Attempt to set the style
+        dom.style.background = prefixes[i] + 'linear-gradient(#000000, #ffffff)';
+        // Detect if the style was successfully set
+        if (dom.style.background)
+        {
+            rtrnVal = prefixes[i];
+        }
+    }
+
+    dom = null;
+    delete dom;
+
+    return rtrnVal;
+  }
